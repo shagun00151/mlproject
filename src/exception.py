f3a68e5cc678210a6fc1233e3e6966ef5b375b51
Shagun Sirohi <shagunsirohi001@gmail.com>
2023-03-18 23:34:32 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() # this variable is used to tell in which file location exception has occured.
